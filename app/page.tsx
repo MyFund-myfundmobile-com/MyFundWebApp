9abc699d32ad9de8627e6ac75b7497e43fe5d18e
Tolulope Ahmed <tolulopeahmed@gmail.com>
2024-08-19 11:56:04 +0100

    Fixing DrTee

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ import Footer from "./ui/landing/footer";
 import Header from "./ui/landing/header";
 import NavBar from "./ui/landing/navbar";
 import HowItWorks from "./ui/landing/steps";
+import RegisterPage from "./register/page";
+import LoginPage from "./login/page";
+import AdminLayout from "./admin/Layout";
+import Layout from "./components/Layout";
+import { Provider } from "react-redux";
 
 const Home = () => {
   return (
