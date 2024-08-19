9cdde254503cf0ef47c2b4d86b6e44be3560c203
Tolulope Ahmed <tolulopeahmed@gmail.com>
2024-08-19 12:20:26 +0100

    Updating staging...

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,16 @@ import Footer from "./ui/landing/footer";
 import Header from "./ui/landing/header";
 import NavBar from "./ui/landing/navbar";
 import HowItWorks from "./ui/landing/steps";
-import RegisterPage from "./register/page";
-import LoginPage from "./login/page";
-import AdminLayout from "./admin/Layout";
-import Layout from "./components/Layout";
-import { Provider } from "react-redux";
 
 const Home = () => {
   return (
-    <Provider store={store}>
+    <>
       <NavBar />
-      <AdminLayout />
-      <Layout />
       <Header />
       <HowItWorks />
       <Facts />
       <Footer />
-      <LoginPage />
-      <RegisterPage />
-    </Provider>
+    </>
   );
 };
 
