ff7b10c8c415f99a534e00d0fb2317720cf5a95f
BamboTims <timilehinadebambo@gmail.com>
2024-09-29 10:11:05 +0100

    fix

--- lib/useWindowWidth.ts
+++ lib/useWindowWidth.ts
@@ -7,19 +7,18 @@ function useWindowWidth() {
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(windowWidth);
+    }; // Initialize with 0 or any default value
+
+    // Call handler once to set the initial width
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
-
-      // Call handler once to set the initial width
-      handleResize();
-
-      // Add event listener
-      window.addEventListener("resize", handleResize);
-
-      // Cleanup event listener on component unmount
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
   }, []);
 
   return windowWidth;
